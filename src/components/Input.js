d07c6f9ea6d86b32ff7c7d10824d2ef4538c019d
ruan65 <ruan65@gmail.com>
2017-12-15 00:55:28 +0300

    initial

---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const input = (props) => {
+
+  return (
+    <div>
+      <label>Input: </label>
+      <input type='text'/>
+      <p>Output</p>
+    </div>
+  )
+}
+
+export default input
